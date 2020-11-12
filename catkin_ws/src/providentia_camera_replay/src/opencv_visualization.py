b13d662c97ce071102f154931c91e42ca7a106c5
brucknem <mbruckner94@gmail.com>
2020-11-12 15:34:37 +0100

    Visualization window open only in first callback

--- catkin_ws/src/providentia_camera_replay/src/opencv_visualization.py
+++ catkin_ws/src/providentia_camera_replay/src/opencv_visualization.py
@@ -28,18 +28,19 @@ class OpenCVVisualization:
 
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber(topic, Image, self.callback)
+        self.window_created = False
 
     def callback(self, data):
-        cv2.namedWindow(self.topic)
-        cv2.moveWindow(self.topic, *self.pos)
+        if not self.window_created:
+            cv2.namedWindow(self.topic)
+            cv2.moveWindow(self.topic, *self.pos)
+            self.window_created = True
+
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
             cv_image = self.process(cv_image, self.size)
         except CvBridgeError as e:
             print(e)
-
-
-
 
     def process(self, cv_image, size):
         cv_image = cv2.resize(cv_image, size)
