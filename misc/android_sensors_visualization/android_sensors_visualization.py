18346212570efe489cc277b78f80638f473ce91d
brucknem <brucknem>
2020-12-01 19:40:56 +0100

    Multiple plots per line or per file

--- misc/android_sensors_visualization/android_sensors_visualization.py
+++ misc/android_sensors_visualization/android_sensors_visualization.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import pandas as pd
 import numpy as np
@@ -42,37 +43,55 @@ def generate_plots(data_folder, output_filename, plots_per_line):
     csv_files = [f for f in csv_files if f.endswith('.csv')]
     csv_files.sort()
 
-    output_file(output_filename)
-    plots = []
+    plots = {}
     for file in csv_files:
-        plots.append(generate_plot(file))
+        plots[file] = generate_plot(file)
 
-    while len(plots) % plots_per_line != 0:
-        plots.append(None)
+    if plots_per_line > 0:
+        while len(plots) % plots_per_line != 0:
+            plots[str(len(plots))] = None
+        output_file('{}.html'.format(output_filename))
+        plots = list(plots.values())
+        plots = list(np.array(plots).reshape(-1, plots_per_line))
+        final_plot = gridplot(plots)
+        show(final_plot)
+        save(final_plot)
+    else:
+        for filename, plot in plots.items():
+            Path(output_filename).mkdir(parents=True, exist_ok=True)
+            filename = os.path.basename(filename)
+            output_file(os.path.join(output_filename, '{}.html'.format(filename.split('.')[0])))
+            show(plot)
+            save(plot)
 
-    plots = list(np.array(plots).reshape(-1, plots_per_line))
-    final_plot = gridplot(plots)
-    show(final_plot)
-    save(final_plot)
+
+help_text = '''
+Usage:   python {}  input_dir   output  [plots_per_line]
+
+        input_dir:          The input directory containing some csv files
+        output:             The output folder   - If no optional or less or equal to 0 plots_per_line is given
+                            The output filename - If plots_per_line is set greater than 0
+        [plots_per_line]:   An optional number of plots per line. 
+                            > 0     - The plots are merged into a single output plot with the given plots per line.
+                            <= 0    - One output file is generated per plot
+'''.format(os.path.basename(__file__))
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 3 or len(sys.argv) > 4:
-        print('Usage:\n\tpython {} {} {} {}'.format(os.path.basename(__file__), '<path_to_folder_with_csvs>',
-                                                 '<output_filename>', '[opt_number_of_plots_per_line]'))
+        print(help_text)
         sys.exit(1)
-        
+
     data_folder = os.path.expanduser(sys.argv[1])
     output_filename = os.path.expanduser(sys.argv[2])
     if '.' in output_filename:
         output_filename = output_filename.split('.')[0]
-    output_filename += '.html'
 
-    plots_per_line = 3
+    plots_per_line = -1
     if len(sys.argv) == 4:
         plots_per_line = int(sys.argv[3])
 
     generate_plots(data_folder, output_filename, plots_per_line)
-    
+
     sys.exit(0)
 
